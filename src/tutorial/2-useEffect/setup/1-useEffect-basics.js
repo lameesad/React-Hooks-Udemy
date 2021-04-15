d301246748b0636e197b430e7c4d9c6df2f30f4b
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-15 15:29:09 +0300

    useEffect-Dependency List

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -6,11 +6,20 @@ import React, { useState, useEffect } from "react";
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   useEffect(() => {
-    console.log("call useEffect");
+    console.log("call useEffect only when value is rendered");
     if (value >= 1) {
       document.title = `New Messages(${value})`;
     }
+  }, [value]);
+
+  useEffect(() => {
+    console.log("Hello world will render every time component renders");
   });
+
+  useEffect(() => {
+    console.log("Hello world one time initial render");
+  }, []);
+
   console.log("render component");
   return (
     <>
