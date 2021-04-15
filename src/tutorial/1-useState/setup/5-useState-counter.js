a642458b07c2ca66b605362412d91054fc7330ea
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-15 03:49:40 +0300

    Counter complex increase

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,6 +5,14 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   };
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //  setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
@@ -18,6 +26,14 @@ const UseStateCounter = () => {
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
           increase
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          increase later
         </button>
       </section>
     </>
