13a5b0c2696e3c84eee17aca252faa220050b470
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-26 01:06:16 +0300

    Custom Hooks-useFetch

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+//custom hook
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
 
-export const useFetch = () => {};
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+
+  return { loading, products };
+};
